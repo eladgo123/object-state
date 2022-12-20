6c6094c3df3d1f155a9298642380afbd7caa631c
eladgo123 <eladgolan8@gmail.com>
2022-12-20 17:23:32 +0200

    Record

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import Records from './Records.json';
 function App() {
+  const[toDo , setToDo] = useState(" ")
+  const[toDoList,settoDoList] = useState(['ByMilk', 'Go to a mivie', 'Walk the dog'])
+  
+  function handleTodoChange(e){
+     setToDo(e.target.value)
+  }
+  function AddItem(){
+    settoDoList(toDoList.push(toDo))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input value={toDo} onChange={handleTodoChange}/>
+        <button onClick={AddItem}>Add to liste</button>
+        <h3>Lst of things to do </h3>
+        <ul>
+          {
+            Records.map(
+              Record =>{
+                return(
+                  <div className ='box'>
+                  {Record.name}
+                  </div>
+                )
+              }
+            )
+          }
+          {
+            toDoList.map(
+              toDo => (
+                <li>{toDo}</li>
+              ) )
+          }
+        </ul>
     </div>
   );
 }
